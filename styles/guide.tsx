b43941cd45a32406a4c872edeea2a8d3f2764221
PioterAndrzejewski <109315248+PioterAndrzejewski@users.noreply.github.com>
2023-08-12 14:46:40 +0200

    add styleguide

---
+++ styles/guide.tsx
@@ -0,0 +1,97 @@
+export const styleGuide = {
+  corner: {
+    lg: {
+      borderBottomLeftRadius: 24,
+      borderBottomRightRadius: 24,
+    },
+    sm: {
+      borderRadius: 12,
+    },
+    smAlt: {
+      borderBottomLeftRadius: 12,
+      borderTopLeftRadius: 12,
+      borderTopRightRadius: 12,
+    },
+  },
+  color: {
+    white: "#fff",
+    black: "#1A1A1A",
+    active: "#A8FF76",
+    error: "#FF445A",
+    plum: {
+      "700": "#390273",
+      "500": "#5603ad",
+      "300": "#993afc",
+      "100": "#c692fd",
+    },
+    blue: {
+      "500": "#5603ad",
+      "300": "#b6defd",
+      "100": "#f0f8ff",
+    },
+    gray: {
+      "500": "#9fa2b2",
+      "300": "#bfc1cc",
+      "100": "#d9dae0",
+    },
+  },
+  text: {
+    heading: {
+      "1": {
+        fontFamily: "Poppins, sans-serif",
+        fontWeight: "bold",
+        fontSize: "36px",
+      },
+      "2": {
+        fontFamily: "Poppins, sans-serif",
+        fontWeight: "bold",
+        fontSize: "28px",
+      },
+      "3": {
+        fontFamily: "Poppins, sans-serif",
+        fontWeight: "bold",
+        fontSize: "22px",
+      },
+      "4": {
+        fontFamily: "Poppins, sans-serif",
+        fontWeight: "semibold",
+        fontSize: "16px",
+      },
+    },
+    button: {
+      fontFamily: "Poppins, sans-serif",
+      fontWeight: "semibold",
+      fontSize: "16px",
+      letterSpacing: "1%",
+    },
+    label: {
+      fontFamily: "Poppins, sans-serif",
+      fontWeight: "medium",
+      fontSize: "15px",
+      letterSpacing: "1%",
+    },
+    title: {
+      fontFamily: "Poppins, sans-serif",
+      fontWeight: "medium",
+      fontSize: "15px",
+      lineHeight: "20px",
+    },
+    caption: {
+      fontFamily: "Poppins, sans-serif",
+      fontWeight: "medium",
+      fontSize: "15px",
+      lineHeight: "20px",
+    },
+    body: {
+      fontFamily: "SF Compact Text, sans-serif",
+      fontWeight: "regular",
+      fontSize: "14px",
+    },
+    special: {
+      fontFamily: "SF Compact Text, sans-serif",
+      fontWeight: "regular",
+      fontSize: "12px",
+      lineHeight: "16px",
+    },
+  },
+};
